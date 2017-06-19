d41b888e21df33bb87e43d2c4e8ba333018a12c2
deepanshujain92 <deepanshu.gv@gmail.com>
2017-06-19 18:09:52 +0530

    Change main to server.js

--- server.js
+++ server.js
@@ -49,32 +49,31 @@ app.post('/webhook', function (req, res) {
 
 
 
-    }
-    else if (action === 'ministatement' && body.result.actionIncomplete === false)
+    } else if (action === 'ministatement' && body.result.actionIncomplete === false)
     {
         var request = require("request");
 
-        var options = { 
-  method: 'POST',
-  url: 'http://52.172.213.166:8080/sbi/Mini/api/EnqMiniStatement',
-  headers: 
-   { 
-     'cache-control': 'no-cache',
-      apikey: 'ywr4rQdbjSOVDtr' ,
-     'content-type': 'application/json'
- },
-     body: { AccountNumber: "00000030001512992" },
-     json:true
- };
+        var options = {
+            method: 'POST',
+            url: 'http://52.172.213.166:8080/sbi/Mini/api/EnqMiniStatement',
+            headers:
+                    {
+                        'cache-control': 'no-cache',
+                        apikey: 'ywr4rQdbjSOVDtr',
+                        'content-type': 'application/json'
+                    },
+            body: {AccountNumber: "00000030001512992"},
+            json: true
+        };
         request(options, function (error, response, body) {
             if (error)
                 throw new Error(error);
-             console.log(body);
-            var speech = "Sure, Your current balance is "+body.CurrentBalance+". Here is your last 5 transactions  :";
-            for(var i = 0;i<body.StatementInfo.length;i++)
+            console.log(body);
+            var speech = "Sure, Your current balance is " + body.CurrentBalance + ". Here is your last 5 transactions  :";
+            for (var i = 0; i < body.StatementInfo.length; i++)
             {
                 speech = speech + "\n\
-"+(i+1)+". "+body.StatementInfo[i].Amount+" "+body.StatementInfo[i].TransactionDate+" "+body.StatementInfo[i].TransactionType+"\n";
+" + (i + 1) + ". " + body.StatementInfo[i].Amount + " " + body.StatementInfo[i].TransactionDate + " " + body.StatementInfo[i].TransactionType + "\n";
             }
             var json = {
                 "speech": speech,
@@ -85,7 +84,51 @@ app.post('/webhook', function (req, res) {
             res.send(json);
         });
         //console.log(body);
-    }else
+    } 
+    else if (action === 'accntstatement' && body.result.actionIncomplete === false)
+    {
+        var request = require("request");
+        var Period = body.result.parameters.Period;
+        Period = Period.split("/");
+        var options = {
+            method: 'POST',
+            url: 'http://52.172.213.166:8080/sbi/Detail/api/EnqINBAccountStatement',
+            headers:
+                    {
+                        'cache-control': 'no-cache',
+                        apikey: 'ywr4rQdbjSOVDtr',
+                        'content-type': 'application/json'
+                    },
+            body: {
+                "AccountNumber": "30001512992", 
+                "FromAmount": body.result.parameters.FromAount, 
+                "FromDate": "0", 
+                "ToAmount": body.result.parameters.ToAmount, 
+                "ToDate": "0", "TransactionNumber": "150"
+            },
+            json: true
+        };
+        request(options, function (error, response, body) {
+            if (error)
+                throw new Error(error);
+            console.log(body);
+            var speech = "Sure, Your account  balance is " + body.CurrentBalance + ". Here is your last 5 transactions  :";
+            for (var i = 0; i < body.StatementInfo.length; i++)
+            {
+                speech = speech + "\n\
+" + (i + 1) + ". " + body.StatementInfo[i].Amount + " " + body.StatementInfo[i].TransactionDate + " " + body.StatementInfo[i].TransactionType + "\n";
+            }
+            var json = {
+                "speech": speech,
+                "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+                "source": "DuckDuckGo"
+            };
+            res.set("Content-type", "application/json");
+            res.send(json);
+        });
+        //console.log(body);
+    }
+    else
     {
         var json = {
             "speech": body.result.fulfillment.speech,
